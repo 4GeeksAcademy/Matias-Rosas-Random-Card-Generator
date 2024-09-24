e0ce2092707ee0bf324a86be18e4405f88972c80
MatiRosas31 <165863634+MatiRosas31@users.noreply.github.com>
2024-09-24 01:07:23 +0000

    [Proyecto completado] -Se generaron 5 clases especificas: corner top botton middle y cornerred -las clases "top" y "bottom" se utilizaron para colocar "centrar" los simbolos de las cartas en las puntas/esquinas. Usando "top" y "left" Tambien en "bottom" se utilizo la propiedad "transform" para rotar 180 grados el simbolos -Cornnerred simplemente se uso para ser agregado en el js cuando se tratase de un corazon o rombo. -En Js se crearon 2 arrays. Uno con los simbolos y otro con los numeros/letras - Luego se "targeteo" cada elemento html a ser modificado. Y se cambio las propiedad "texContent" de cada span ubicado en las puntas. Y el "texContent" del numero/letra central. -Se agrego un if para identificar/evaluar cuando si se tratase de un corazon o rombo. Y de ser asi agregar la clase cornerred al div en donde se encontraba dicho simbolo. Y asi cambiar el color.

--- src/app.js
+++ src/app.js
@@ -7,4 +7,36 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  const palos = [`♦`, `♥`, `♠`, `♣`];
+  const values = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "j",
+    "q",
+    "k",
+    "a"
+  ];
+
+  let cornerdivtophtml = document.querySelector("#cornerdivtop");
+  let cornerspantophtml = document.querySelector("#cornerspantop");
+  let cornerspanbothtml = document.querySelector("#cornerspanbot");
+  let cornerdivbothtml = document.querySelector("#cornerdivbot");
+  let midspannumberhtml = document.querySelector("#midspannumber");
+  let randomPalo = palos[Math.floor(Math.floor(Math.random() * palos.length))];
+  let randomvalue =
+    values[Math.floor(Math.floor(Math.random() * values.length))];
+  cornerspantophtml.textContent = `${randomPalo}`;
+  cornerspanbothtml.textContent = `${randomPalo}`;
+  midspannumberhtml.textContent = `${randomvalue}`;
+  if (randomPalo === "♥" || randomPalo === "♦") {
+    cornerdivbothtml.classList.add("cornerred");
+    cornerdivtophtml.classList.add("cornerred");
+  }
 };
